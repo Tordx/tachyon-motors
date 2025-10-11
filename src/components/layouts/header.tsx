35d135705a142a96e25acf95cbfce6f0f0001bb0
Tordx <102406588+Tordx@users.noreply.github.com>
2025-10-11 22:36:48 +0800

    remove blur on home

--- src/components/layouts/header.tsx
+++ src/components/layouts/header.tsx
@@ -11,9 +11,9 @@ import MobileNavigator from '../routing/mobile-navigator'
 const Header = () => {
   const currentPath = usePathname();
   const [isNavOpen, setIsNavOpen] = React.useState(false);
-
+  console.log(currentPath)
   return (
-    <header className={`w-full fixed top-0 left-0 z-50 ${currentPath === 'home' ? '' : 'backdrop-blur-md'}`}>
+    <header className={`w-full fixed top-0 left-0 z-50 ${currentPath === '/home' ? '' : 'backdrop-blur-md'}`}>
       <div className='w-full h-16 px-10 2xl:px-auto bg-transparent text-white flex items-center justify-center sm:justify-between'>
 
         <div className='z-50 flex flex-col items-center cursor-pointer gap-4 mt-15 sm:mt-0'>
