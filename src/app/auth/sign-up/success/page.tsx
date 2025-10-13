16bc728386ec9f07b35c8ad703d7b744f0bacaee
Tordx <102406588+Tordx@users.noreply.github.com>
2025-10-14 03:54:05 +0800

    suspensed wrapped

--- src/app/auth/sign-up/success/page.tsx
+++ src/app/auth/sign-up/success/page.tsx
@@ -1,41 +1,10 @@
-'use client'
+import { Suspense } from 'react'
+import SignUpSuccess from './client'
 
-import { useEffect, useState } from 'react'
-import { useRouter, useSearchParams } from 'next/navigation'
-import AuthButton from '@/components/molecules/auth-buttons'
-import { useWindowWidth } from '@/utils/use-window-width'
-import MailBox from '@/components/illustrations/mailbox'
-
-export default function SignUpSuccess() {
-  const router = useRouter()
-  const searchParams = useSearchParams()
-  const email = searchParams.get('email')
-  const [isValid, setIsValid] = useState(false)
-  const width = useWindowWidth();
-
-  useEffect(() => {
-    const cameFromSignup = sessionStorage.getItem('signupSuccess')
-    if (cameFromSignup === 'true') {
-      setIsValid(true)
-      sessionStorage.removeItem('signupSuccess') // prevent re-entry
-    } else {
-      router.replace('/auth/login') // 🚫 direct visits get bounced
-    }
-  }, [router])
-
-  if (!isValid) return null
-
+export default function SignUpSuccessPage() {
   return (
-    <div className="text-center p-10 font-montserrat">
-      <MailBox size={width <= 768 ? "" :"500"} />
-      <h2 className="text-3xl font-semibold text-green-600 mt-4">🎉 Account Created!</h2>
-      <p className="mt-2 text-gray-400">
-        We’ve sent a verification link to <span className="text-white">{email}</span>.<br />
-        Please check your inbox to verify your account before logging in.
-      </p>
-      <div className="mt-6">
-        <AuthButton onClick={() => router.push('/login')}>Go to Login</AuthButton>
-      </div>
-    </div>
+    <Suspense fallback={<div>Loading...</div>}>
+      <SignUpSuccess />
+    </Suspense>
   )
 }
